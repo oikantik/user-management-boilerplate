b33da9988d74daf5d8cfb50aa7f3c80f50812b4e
Nahid Hossain <nahid@Nahids-MacBook-Pro.local>
2020-04-23 18:54:53 -0400

    added header to axios

--- src/redux/middleware/register.js
+++ src/redux/middleware/register.js
@@ -4,6 +4,9 @@ import { registerUserURI } from "../../config/default";
 export default async (payload) => {
   const response = await axios.post(registerUserURI, {
     data: payload,
+    headers: {
+      "Content-Type": "application/json",
+    },
   });
   return {
     id: response.data.id,
